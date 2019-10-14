e6e94aa992072313fa116e0bc9e7566c7ab1fb56
9ppf <48794672+9ppf@users.noreply.github.com>
2019-10-14 23:08:41 +0800

    tabbar（底部面板）

---
+++ demo/App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		// 当uni-app 初始化完成时触发（全局只触发一次）
+		onLaunch: function () {
+			console.log('App Launch')
+		},
+		// 当 uni-app 启动，或从后台进入前台显示
+		onShow: function () {
+			console.log('App Show')
+		},
+		// 当 uni-app 从前台进入后台
+		onHide: function () {
+			console.log('App Hide')
+		},
+		// 对 nvue 页面发送的数据进行监听，
+		onUniNViewMessage:function(){}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ demo/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="content">
+      首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+           console.log("index")
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+    .logo{
+        height: 200upx;
+        width: 200upx;
+        margin-top: 200upx;
+    }
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
